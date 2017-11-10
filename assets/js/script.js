d98b8b779334efc5e160ce75ff5742e065cd1ea8
Jack G <jackgordon@berkeley.edu>
2017-11-09 18:07:45 -0800

    test

---
+++ assets/js/script.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+
+	$(".premiere-more").click(function(){
+
+		if ($("#premiere-section-2").hasClass("premiere-section-2-inactive")){
+			$("#premiere-section-2").addClass("premiere-section-2-active");
+			$("#premiere-section-2").removeClass("premiere-section-2-inactive");
+			$("#premiere-section-2").css("display", "block");
+		}
+		else {
+			$("#premiere-section-2").addClass("premiere-section-2-inactive");
+			$("#premiere-section-2").removeClass("premiere-section-2-active");
+			$("#premiere-section-2").css("display", "none");
+		}
+	})
+	
+	$(".donate-button").click(function(){
+
+			if ($(this).is("#cash-but")) {
+					
+					$("#cash").css("display", "inline-block");
+					$("#online").css("display", "none");
+					$("#question").css("display", "none");
+				} 
+			else if ($(this).is("#online-but")) {
+					$("#cash").css("display", "none");
+					$("#online").css("display", "inline-block");
+					$("#question").css("display", "none");
+				} 
+			else {
+					$("#cash").css("display", "none");
+					$("#online").css("display", "none");
+					$("#question").css("display", "inline-block");
+			}
+
+		
+
+		})
+
+	$("#trailer-box").click(function(){
+
+        $('html, body').animate({
+            scrollTop: $('#trailer-section').position().top },
+            1000
+        );
+    })
+
+	
+	$("#premiere-box").click(function(){
+
+		$('html, body').animate({
+            scrollTop: $('#premiere-section').position().top },
+            1000
+        );
+
+	})
+		
+
+		})
+
+
+		
